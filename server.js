e95e68df2f8fd6c6584320df146ad61d217419dc
Vince8459 <8459vinnymaina@gmail.com>
2023-01-18 12:20:45 +0300

    Travis Test1

---
+++ server.js
@@ -0,0 +1,13 @@
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function main() {
+  while(true) {
+    console.log("My Favourite food is ${FAVOURITE_FOOD");
+    await sleep(5000);
+  }
+}
+
+main();
